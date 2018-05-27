c9bbbe45039b61fd0bea8a87f7044881f129c1e3
Ravi Kishore Thella <ravikishorethella@gmail.com>
2018-05-26 21:26:13 -0400

    stored values from the form

--- js/app.js
+++ js/app.js
@@ -1,3 +1,22 @@
 // instantiating the classes
 const cryptoAPI = new CryptoAPI();
 const ui = new UI();
+
+// variables
+const form = document.getElementById("form");
+
+// event listeners
+form.addEventListener("submit", e => {
+  e.preventDefault();
+
+  //   reading the values from the form
+  const currencySelect = document.getElementById("currency").value;
+  const cryptoCurrencySelect = document.getElementById("cryptocurrency").value;
+
+  //   validating the form
+  if (currencySelect === "" || cryptoCurrencySelect === "") {
+    // displaying an error
+  } else {
+    // correct info
+  }
+});
